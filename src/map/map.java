41c05a8b3ec2a011a5510a09781edab5e9daeace
Naixsys <naixsys@protonmail.com>
2020-01-28 16:01:27 -0500

    Added positioning to the map class

--- src/map/map.java
+++ src/map/map.java
@@ -1,8 +1,68 @@
 package map;
 
-public class map{
+import java.util.Random;
 
-    public map () {
-        
+import player.player;
+import wumpus.wumpus;
+
+public class map{
+    protected Random rander = new Random();
+    protected String [][] map_grid;
+
+    public map ()
+     {
+    
+        int x_size = rander.nextInt(5);
+        int y_size = rander.nextInt(5);
+        map_grid = new String [x_size] [y_size];
+
     }
+
+    public void setRandomPosition(wumpus wumpus)
+    {   
+        while (true)
+        {
+            String position_of[][];
+            position_of = map_grid;
+            int xpos = rander.nextInt(map_grid.length);
+            int ypos = rander.nextInt(map_grid[0].length); 
+            if (position_of[xpos][ypos] == null)
+            {
+            position_of[xpos][ypos] = ("Wumpus");
+            break;
+            }
+
+            else 
+             {
+                continue;
+             }
+        }
+    }
+
+    public void setRandomPosition(player player)
+    {
+        boolean runner = true;
+        while (runner)
+        {
+            String position_of[][];
+            position_of = map_grid;
+            int xpos = rander.nextInt(map_grid.length);
+            int ypos = rander.nextInt(map_grid[0].length);
+            if (position_of[xpos][ypos] == null)
+            {
+            position_of[xpos][ypos] = "Player";
+            runner = false;
+            }
+
+            else 
+             {
+                continue;
+             }
+        }
+    }
+    /*public int getXSize()
+    {   
+        System.out.println(map_grid.length);
+        return map_grid.length;
+    } */
 }
--- src/wumpus/wumpus.java
+++ src/wumpus/wumpus.java
@@ -1,18 +1,29 @@
 package wumpus;
-
-public class wumpus {
+import map.map;
+public class wumpus extends map {
 
     private boolean life = true;
-    
-    public wumpus(){
-
+    private map local_map;
+    public wumpus(map spawn_map)
+    {
+        local_map = spawn_map;
+        local_map.setRandomPosition(this);
+        System.out.println("Spawned Wumpus\n");
     }
     
-    public boolean kill_wumpus () {
-
+    public boolean kill_wumpus () 
+    {
         life = false;
-
         return life;
     }
 
+   /* public void moveWumpus()
+    {
+        local_map.moveWumpus(this);
+    }*/
+    public static void main(String[] args) {
+        map map = new map();
+        wumpus wumpus = new wumpus(map);
+
+    }
 }
